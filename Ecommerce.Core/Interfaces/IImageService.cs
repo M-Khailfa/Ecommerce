140e8d20e1cb44d1cc28719a66afab1396707c10
Mahmoud Emad Khalifa <mahmoudkhalifaaa97@gmail.com>
2026-05-13 13:18:55 +0300

    Adding Image Service Using Cloudinary

---
+++ Ecommerce.Core/Interfaces/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Core.Interfaces
+{
+    public interface IImageService
+    {
+        Task<string> UploadImageAsync(IFormFile file);
+        Task<bool> DeleteImageAsync(string publicId);
+    }
+}
---
+++ Ecommerce.Infrastructure/Repos/ImageService.cs
@@ -0,0 +1,60 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Ecommerce.Core.Interfaces;
+using Ecommerce.Core.Settings;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Ecommerce.Infrastructure.Repos
+{
+    public class ImageService : IImageService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public ImageService(IOptions<CloudinarySettings> options)
+        {
+            var account = new Account(
+                options.Value.CloudName,
+                options.Value.ApiKey,
+                options.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+            _cloudinary.Api.Secure = true;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return "";
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(file.FileName, file.OpenReadStream()),
+                PublicId = Guid.NewGuid().ToString()
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+            if (uploadResult.StatusCode == HttpStatusCode.OK)
+            {
+                return uploadResult.SecureUrl.ToString();
+            }
+
+            return "";
+        }
+
+        public async Task<bool> DeleteImageAsync(string publicId)
+        {
+            var deletionParams = new DeletionParams(publicId)
+            {
+                ResourceType = ResourceType.Image
+            };
+            var result = await _cloudinary.DestroyAsync(deletionParams);
+            return result.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}
